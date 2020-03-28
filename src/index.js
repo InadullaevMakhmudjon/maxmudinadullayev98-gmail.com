92f33012e3f8afc364fe88ecfb81661e29a0bd19
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-03-28 10:38:50 +0500

    Bot configured for launching

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Working fine');
+import Telegraf from 'telegraf';
+import session from 'telegraf/session';
+import commands from './commands';
+import hears from './hears';
+
+const bot = new Telegraf('982165968:AAFvf5OH2iEkdJCt1oT8f0T_1OdRjasGjjY');
+bot.use(session());
+
+commands(bot);
+hears(bot);
+
+bot.launch().then(() => {
+  console.log('\x1b[34m', 'Launched', '\x1b[0m');
+});
