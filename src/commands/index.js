dbbdeb6f65979040442def6097bf3d979ac59cc9
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-03-28 10:37:38 +0500

    Start command created

---
+++ src/commands/index.js
@@ -0,0 +1,8 @@
+import keyboards from '../keyboards';
+import { start as startMessage } from '../messages';
+
+export default (bot) => {
+  bot.command('start', (ctx) => {
+    ctx.replyWithHTML(startMessage(ctx.from.first_name), keyboards.start);
+  });
+};
