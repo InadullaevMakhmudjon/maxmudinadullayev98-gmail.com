87ab2f8b48e16009627b2a10026e817df5ad0598
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-03-29 09:52:46 +0500

    Back arrays created

--- src/keyboards/back.js
+++ src/keyboards/back.js
@@ -1,4 +1,6 @@
 import Markup from 'telegraf/markup';
 import locale from '../locale/index.json';
 
+export const custom = (lang) => [{ text: locale.back[lang] }, { text: locale.shopping[lang] }];
+
 export default (lang) => [Markup.button(locale.back[lang]), Markup.button(locale.shopping[lang])];
