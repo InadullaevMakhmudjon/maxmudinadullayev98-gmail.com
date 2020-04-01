65a02f09a2da90682e85aabcddfe9fb7e08e5b84
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-01 17:21:42 +0500

    Agremment message created

---
+++ src/messages/agreement.js
@@ -0,0 +1,31 @@
+import locale from '../locale/index.json';
+
+const total = (product) => Number(product.quantity) * Number(product.price);
+
+function getProduct(product) {
+  return `
+${product.name}: 
+    ${product.quantity} X ${product.price} = ${total(product)}
+`;
+}
+
+function getTotal(products) {
+  return products.map((product) => total(product)).reduce((a, b) => a + b, 0);
+}
+
+export default (lang, fullName, phone, dType, comment, products) => `
+${locale.messageCustomer[lang]}: ${fullName}
+${locale.messagePhonenumber[lang]}: ${phone}
+${locale.messageDelivery[lang]}: ${dType}
+${locale.messageComment[lang]}: ${comment}
+
+~~~~~~~~~~~~~~~~~~~
+${
+  products.map((product) => getProduct(product)).join('')
+}
+~~~~~~~~~~~~~~~~~~~
+
+${locale.messageSum[lang]}: ${getTotal(products)}
+${locale.messageDeliveryPrice[lang]}: 0
+${locale.messageTotal[lang]}: ${getTotal(products)}
+`;
