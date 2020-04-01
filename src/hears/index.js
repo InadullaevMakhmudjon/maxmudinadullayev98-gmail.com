0e3ce2c5610f3d44a445301bc06c32c9608c2553
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-01 17:28:09 +0500

    Sessions and index hears imported other components

--- src/hears/index.js
+++ src/hears/index.js
@@ -7,29 +7,67 @@ import dataSomsa from '../data/somsa.json';
 import dataDrinks from '../data/drinks.json';
 import productSomsa from './productSomsa';
 import productDrink from './productDrinks';
+import selectedQuantity from './selectedQuantity';
+import shopping from './shopping';
+import deleteProduct from './deleteProduct';
+import order from './OrderShopping';
+import delivery from './delivery';
+import * as types from '../listeners/state.types';
+import publish from './publish';
+
+require('dotenv').config();
 
 const toArray = (object) => (object ? Object.keys(object).map((key) => object[key]) : ["🇺🇿 O'zbekcha", '🇷🇺 Русский']);
 const arrayFiltered = (object) => toArray(Object.keys(object).filter((key) => (key === 'name' || key === 'name_uz' || key === 'name_ru')).map((key) => object[key]));
 
-const trace = (callBack, ctx) => {
+const back = (ctx) => {
+  const array = ctx.session.back;
+  if (array) {
+    switch (ctx.session.state) {
+      case types.ON_COMMENT:
+        ctx.session.state = '';
+        order(ctx);
+        break;
+      default:
+        array[array.length - 1](ctx);
+        array.pop();
+    }
+  }
+};
+
+export const trace = (callBack, ctx) => {
   if (!ctx.session.back) {
     ctx.session.back = [];
   }
   ctx.session.back.push(callBack);
 };
 
+const clear = (ctx, { store }) => {
+  const { language } = ctx.session;
+  ctx.session = null;
+  store.clear();
+  return language;
+};
+
 export default (bot) => {
+  // Main
   bot.hears(toArray(null), (ctx) => {
-    locale(ctx); // Opens main
+    locale(ctx);
   });
+
+  // Drinks
   bot.hears(toArray(lang.drinks), (ctx) => {
     trace(locale, ctx);
     drinks(ctx);
   });
+
+  // Somsa
   bot.hears(toArray(lang.somsa), (ctx) => {
     trace(locale, ctx);
     somsa(ctx);
   });
+
+  // If any somsa selected
   bot.hears(
     dataSomsa.map((data) => arrayFiltered(data)).reduce((a, b) => a.concat(b), []),
     (ctx) => {
@@ -37,6 +75,8 @@ export default (bot) => {
       productSomsa(ctx);
     },
   );
+
+  // If any drink selected
   bot.hears(
     dataDrinks.map((drink) => arrayFiltered(drink)).reduce((a, b) => a.concat(b), []),
     (ctx) => {
@@ -44,11 +84,62 @@ export default (bot) => {
       productDrink(ctx);
     },
   );
-  bot.hears(toArray(lang.back), (ctx) => {
-    const array = ctx.session.back;
-    if (array) {
-      array[array.length - 1](ctx);
-      array.pop();
-    }
+
+  // If quantity selected
+  bot.hears(Array.from(Array(10).keys()).map((el) => el.toString()), (ctx) => {
+    selectedQuantity(ctx);
   });
+
+  // Shopping
+  bot.hears((text) => text.includes(lang.shopping.uz) || text.includes(lang.shopping.ru),
+    (ctx) => {
+      trace(locale, ctx);
+      shopping(ctx);
+    });
+
+  // Clear
+  bot.hears(toArray(lang.clear), (ctx) => {
+    ctx.session.shopping = null;
+    shopping(ctx);
+  });
+
+  // Delete product
+  bot.hears((text) => text.includes('❌'), (ctx) => {
+    deleteProduct(ctx);
+    shopping(ctx);
+  });
+
+  // Order
+  bot.hears(toArray(lang.order), (ctx) => {
+    trace(shopping, ctx);
+    order(ctx);
+  });
+
+  // Courier type delivery
+  bot.hears(toArray(lang.courierType), (ctx) => {
+    trace(order, ctx);
+    delivery(ctx, 0);
+  });
+
+  // Self type delivery
+  bot.hears(toArray(lang.selfType), (ctx) => {
+    trace(order, ctx);
+    delivery(ctx, 1);
+  });
+
+  // Approve delivery
+  bot.hears(toArray(lang.approve), (ctx) => {
+    publish(ctx, bot);
+    const language = clear(ctx, bot.store);
+    locale(ctx, language);
+  });
+
+  // Reject delivery
+  bot.hears(toArray(lang.reject), (ctx) => {
+    const language = clear(ctx, bot.store);
+    locale(ctx, language);
+  });
+
+  // Back
+  bot.hears(toArray(lang.back), (ctx) => back(ctx));
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import Telegraf from 'telegraf';
 import session from 'telegraf/session';
 import commands from './commands';
 import hears from './hears';
+import listeners from './listeners';
 
-const bot = new Telegraf('982165968:AAFvf5OH2iEkdJCt1oT8f0T_1OdRjasGjjY');
-bot.use(session());
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.TOKEN);
+
+bot.store = { store: new Map() };
+
+bot.use(session({ ...bot.store }));
 
 commands(bot);
 hears(bot);
+listeners(bot);
 
 bot.launch().then(() => {
   console.log('\x1b[34m', 'Launched', '\x1b[0m');
