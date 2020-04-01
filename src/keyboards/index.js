40d48f21e615d9124b7109c74d7034984b4eea57
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-01 17:24:10 +0500

    All keyboards exported

--- src/keyboards/index.js
+++ src/keyboards/index.js
@@ -1,9 +1,21 @@
 import start from './start';
 import main from './main';
 import back from './back';
+import order from './order';
+import delivery from './delivery';
+import contact from './contact';
+import approve from './approve';
+
+export const count = (ctx) => (ctx.session.shopping
+  ? `(${ctx.session.shopping.reduce((a, b) => a + Number(b.quantity), 0)})`
+  : '');
 
 export default {
   start,
-  main: (ctx) => main(ctx.session.language),
-  back: (ctx) => back(ctx.session.language),
+  main: (ctx) => main(ctx.session.language, count(ctx)),
+  back: (ctx) => back(ctx.session.language, count(ctx)),
+  order: (ctx) => order(ctx.session.language),
+  delivery: (ctx, type) => delivery(ctx.session.language, type),
+  contact: (ctx) => contact(ctx, ctx.session.language),
+  approve: (ctx) => approve(ctx.session.language),
 };
