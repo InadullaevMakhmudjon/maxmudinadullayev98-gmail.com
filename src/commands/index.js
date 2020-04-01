34e42a790cde1b3887c49e2c8f5d27826d5e56f8
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-01 17:27:27 +0500

    Start command modified

--- src/commands/index.js
+++ src/commands/index.js
@@ -3,6 +3,7 @@ import { start as startMessage } from '../messages';
 
 export default (bot) => {
   bot.command('start', (ctx) => {
+    ctx.session = {};
     ctx.replyWithHTML(startMessage(ctx.from.first_name), keyboards.start);
   });
 };
