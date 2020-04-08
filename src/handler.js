a7f7b8dd13a15230b4b1fb60ade6aef9fbb31255
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-08 17:10:31 +0500

    Bot sessions updated

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,12 @@
 import message from './messages/errorMessage';
 
 function checker(ctx) {
-  if (Object.keys(ctx.session).length) return true;
-  if (ctx.update.message.text === '/start') return true;
-  if (ctx.update.message.text === "🇺🇿 O'zbekcha") return true;
-  if (ctx.update.message.text === '🇷🇺 Русский') return true;
+  if (ctx.session) {
+    if (Object.keys(ctx.session).length) return true;
+    if (ctx.update.message.text === '/start') return true;
+    if (ctx.update.message.text === "🇺🇿 O'zbekcha") return true;
+    if (ctx.update.message.text === '🇷🇺 Русский') return true;
+  }
   return false;
 }
 
