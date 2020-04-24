050bf1b13e77e55685bb03d9c28bc309da876d8b
root <root@localhost.localdomain>
2020-04-24 16:19:26 +0000

    Some bugs fixed made by user

--- src/handler.js
+++ src/handler.js
@@ -3,9 +3,9 @@ import message from './messages/errorMessage';
 function checker(ctx) {
   if (ctx.session) {
     if (Object.keys(ctx.session).length) return true;
-    if (ctx.update.message.text === '/start') return true;
-    if (ctx.update.message.text === "🇺🇿 O'zbekcha") return true;
-    if (ctx.update.message.text === '🇷🇺 Русский') return true;
+    if(ctx.update.message
+      && ['/start', "🇺🇿 O'zbekcha", '🇷🇺 Русский']
+      .includes(ctx.update.message.text)) return true;
   }
   return false;
 }
--- src/messages/start.js
+++ src/messages/start.js
@@ -1,6 +1,19 @@
 export default (username) => `
-Assalomu alaykum!
+
+Assalomu alaykum!!
 Здравствуйте, <b>${username}</b>
+
+Поздравляем вас от лица команды Grand Somsa Markazi с наступлением священного месяца Рамадан 🌙
+
+с 24.04.2020 по 23.05.2020 
+режим работы: 17:00-04:00
+
+Sizni kirib kelgan muqaddas Ramazon oyi munosabati bilan Grand Somsa Markazi jamoasi nomidan tabriklaymiz.🌙
+
+24.04.2020 dan 23.05.2020 gacha
+ish vaqti: 17:00-04:00
+
 🇺🇿 Tilni tanlang
 🇷🇺 Выберите язык
 `;
+
