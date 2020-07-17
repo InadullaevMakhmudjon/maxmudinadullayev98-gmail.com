a230fb5da7c40f6602ba7d2cfd68c8d3c1c455f8
root <root@localhost.localdomain>
2020-07-17 13:55:48 +0000

    Order type takes only delivery one

--- src/hears/OrderShopping.js
+++ src/hears/OrderShopping.js
@@ -1,6 +1,8 @@
 import buttons from '../keyboards';
 import locale from '../locale/index.json';
+import delivery from './delivery';
 
 export default (ctx) => {
-  ctx.reply(locale.order[ctx.session.language], buttons.order(ctx));
+  // ctx.reply(locale.order[ctx.session.language], buttons.order(ctx));
+  delivery(ctx, 0)
 };
